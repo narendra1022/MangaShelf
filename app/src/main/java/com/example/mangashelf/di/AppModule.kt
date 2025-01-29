9f90b364843d1c3608b18a3c47780939f5b08deb
Narendra <narendramadugula2004@gmail.com>
2025-01-29 23:33:20 +0530

    DI-Dependency Injection Implemented

---
+++ app/src/main/java/com/example/mangashelf/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.example.mangashelf.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.mangashelf.data.local.MangaDatabase
+import com.example.mangashelf.data.remote.MangaApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideMangaApi(): MangaApi {
+
+        return Retrofit.Builder()
+            .baseUrl("https://www.jsonkeeper.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MangaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMangaDatabase(
+        @ApplicationContext context: Context
+    ): MangaDatabase {
+        return Room.databaseBuilder(
+            context,
+            MangaDatabase::class.java,
+            "manga_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMangaDao(database: MangaDatabase) = database.mangaDao()
+}
